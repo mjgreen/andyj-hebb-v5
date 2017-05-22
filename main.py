72294bfc83c902bdb5ef8f534c140b7843fc131d
matt green <mjglab@gmail.com>
2017-05-22 13:27:31 +0100

    removed duplicate statement

--- main.py
+++ main.py
@@ -20,9 +20,6 @@ win = prelims.showInstructions(verbose=verbose, debug=debug)
 # choose stimuli
 m1,m2,f = prelims.chooseStimuli(verbose=verbose, debug=debug)
 
-# block count starts at zero which indicates a practiice block
-blockCount=0
-
 # run one block for practice
 isPractice = block.doOneBlock(verbose=verbose, debug=debug, isPractice=isPractice,
                               m1=m1,m2=m2,f=f, win=win, blockCount=blockCount)
